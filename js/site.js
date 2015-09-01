62da002425807794fda4fb8ec974458341b39677
Jonas Platte <mail@jonasplatte.de>
2015-09-01 20:52:19 +0200

    Aaahhgit status Tabs!

--- js/site.js
+++ js/site.js
@@ -3,14 +3,14 @@
         var externalSitesCheckbox = $("#external-sites-dd");
 
         $(document).click(function() {
-    		// deactivate external sites dropdown if it's active
-    		externalSitesCheckbox.prop("checked", false);
-    	});
+            // deactivate external sites dropdown if it's active
+            externalSitesCheckbox.prop("checked", false);
+        });
 
-    	$(".external-sites-icons-v").click(function(ev) {
-    		// without this the external-sites-dd checkbox would
-    		// deactive itself through the above click handler
-    		ev.stopPropagation();
-    	});
+        $(".external-sites-icons-v").click(function(ev) {
+            // without this the external-sites-dd checkbox would
+            // deactive itself through the above click handler
+            ev.stopPropagation();
+        });
     });
 })(jQuery);
